602de5e54186eb6781bd747dfc1543c2c330085e
Al hussein Khouma/Full Stack Developper <68778997+moustapha96@users.noreply.github.com>
2025-11-27 11:38:31 +0000

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -30,6 +30,7 @@
         'views/rental_payment_schedule_views.xml',
         'views/gestion_magasin_config_views.xml',
         'views/account_move_rental_payment_views.xml',
+        'views/invoice_reminder_history_views.xml',
         'views/rental_menu.xml',
         'views/res_partner_view.xml',
         'data/mail_templates.xml'
